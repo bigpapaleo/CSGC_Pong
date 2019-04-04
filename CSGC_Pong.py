f4d01f359d62d87e29f796d4cbddb55dcf76870b
Daniel Jackson <Daniel@createsomethinginteresting.com>
2019-04-03 19:03:54 -0500

    The version that I'm submitting to CSGC

--- CSGC_Pong.py
+++ CSGC_Pong.py
@@ -1,6 +1,8 @@
 #pongtest
 import turtle
 import time
+import winsound
+
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
@@ -27,7 +29,7 @@ score_b = 0
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("white")
+paddle_a.color("green")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
@@ -36,7 +38,7 @@ paddle_a.goto(-350, 0)
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("white")
+paddle_b.color("red")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
@@ -45,7 +47,7 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
-ball.color("white")
+ball.color("blue")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = .25
@@ -54,7 +56,7 @@ ball.dy = -.25
 #Pen
 pen = turtle.Turtle()
 pen.speed(0)
-pen.color("white")
+pen.color("purple")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
@@ -100,10 +102,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("floop.wav", winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("floop.wav", winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -111,6 +115,7 @@ while True:
         score_a += 1
         pen.clear()
         pen.write("{}   {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("rubber_ducky.wav", winsound.SND_ASYNC)
         
     if ball.xcor() < -390:
         ball.goto(0, 0)
@@ -118,15 +123,18 @@ while True:
         score_b += 1
         pen.clear()
         pen.write("{}   {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("rubber_ducky.wav", winsound.SND_ASYNC)
 
     #paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50):
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound("boing_x.wav", winsound.SND_ASYNC)
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50):
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound("boing_x.wav", winsound.SND_ASYNC)
 
 
 
